51876147901a4b44ecacf9343a2a0b21f029982f
matotella <Syahrizalhamdala2@gmail.com>
2025-06-10 10:59:25 +0700

    evoting

---
+++ project evoting/modul/.ipynb_checkpoints/calon-checkpoint.py
@@ -0,0 +1,21 @@
+ListCalon = []
+
+def tambah_calon():
+    idCal = input("Masukan ID Calon: ")
+    if any(c['id'] == idCal for c in ListCalon):
+        print("ID sudah terdaftar ")
+        return
+    namaCal = input("Masukan Nama Calon: ")
+    visiCal = input("Masukan Nama Visi Misi")
+
+    ListCalon.append({"id": idCal, "nama": namaCal,"visi": visiCal, "jumlah suara":0})
+    print ("calon berhasil didaftarkan")
+
+def get_data():
+    return ListCalon
+
+def cari_calon(id):
+    for c in ListCalon:
+        if c['id'] == id:
+            return c
+    return None
